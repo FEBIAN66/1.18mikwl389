58e26eb18e2ed82917e76b78b9be8609c792e8f4
F3B14N <febiansnapy@gmail.com>
2022-02-25 20:14:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,25 +98,3 @@ bot.on('spawn',function() {
 bot.on('death',function() {
     bot.emit("respawn")
 });
-/* ON TERUS BANG */
-const express = require('express')
-
-const app = express()
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-app.use(express.static("public"));
-
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "./index.html");
-});
-
-app.get("/dreams", (request, response) => {
-  response.json(dreams);
-});
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
